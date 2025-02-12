2ba557efcc1dcb4cd8708093904bc08dd79ab72c
Anton <antonh1803@gmail.com>
2025-02-12 14:38:49 +0200

    fix bug

--- todo/urls.py
+++ todo/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('task-detail-<int:pk>/', views.TaskListDetail.as_view(), name='task_detail'),
     path('new-task', views.CreateTaskView.as_view(), name='add_task'),
     path('<int:pk>/complete', views.TaskCompleteView.as_view(), name='task_complete'),
-    path('task-update-<int:pk>/', views.UpdateTaskView.as_view(), name='task_update'),
-    path('task-delete-<int:pk>/', views.DeleteTaskView.as_view(), name='task_delete'),
+    path('task-update/<int:pk>/', views.UpdateTaskView.as_view(), name='task_update'),
+    path('task-delete/<int:pk>/', views.DeleteTaskView.as_view(), name='task_delete'),
     path("approve-task/", views.ApproveTaskView.as_view(), name="approve_task"),
 
 ]
